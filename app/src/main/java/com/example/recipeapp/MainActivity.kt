1bc06723f550f7a2d9842c9b7c3092f9cdcd8e69
jairahul-r <jairahul.r2022@vitstudent.ac.in>
2024-07-04 20:38:54 +0530

    gradle fix

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -20,5 +20,6 @@ class MainActivity() : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+//        check
+   }
 }
