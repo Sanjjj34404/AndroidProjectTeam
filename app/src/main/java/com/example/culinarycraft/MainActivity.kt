499f83203b6f3a7c71044983d5349f4bbe5c7891
jairahul-r <jairahul.r2022@vitstudent.ac.in>
2024-07-05 16:32:02 +0530

    animation

---
+++ app/src/main/java/com/example/culinarycraft/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.culinarycraft
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.create
+
+class MainActivity() : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        check
+    }
+}
+
+
+
